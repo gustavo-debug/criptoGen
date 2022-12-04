8f2149a17e1b6a044fa2f60c59058d01ed18784c
gustavo-debug <59418992+gustavo-debug@users.noreply.github.com>
2022-12-04 16:04:07 -0300

    primer commit

---
+++ encabezado.js
@@ -0,0 +1,26 @@
+// JavaScript source code
+/*var ancho = window.innerWidth;
+var alto = window.innerHeight;
+*/
+/*
+const entrada = document.querySelector(".sender");
+let ancho = window.onresize;
+var width = window.innerWidth;
+
+window.addEventListener('resize', function () {
+    if (window.innerWidth !== width) {
+        width = window.innerWidth;
+    }
+});
+if (width < 1850) {
+    entrada.setAttribute("placeholder", "buscador Criptomonedas");
+}
+else if (width >= 1850) {
+    entrada.setAttribute("placeholder", "buscar criptomoneda");
+}
+ */
+/*
+if (ancho < 1700) {
+
+    entrada.setAttribute("placeholder", "buscador");
+}*/
